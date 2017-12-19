66412d2dd01212c77e9779b44e198aa12d588607
Chalana <cmkhmer@gmail.com>
2017-12-19 21:33:05 +0100

    Implémentation de ez-draw

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ez-draw.h"
 #include "functions.h"
 
 int main(int argc, char **argv)
 {
-    //test
+    if (ez_init() < 0) exit(1);
+
+    ez_window_create (400, 300, "Demo 01: Hello World", NULL);
+
+    ez_main_loop ();
+    exit(0);
 
 
     return 0;
