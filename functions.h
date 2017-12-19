435314dfe09fac119210e9f0cb1aeacda90997b1
jojiro <34159710+jojiro@users.noreply.github.com>
2017-12-19 22:08:36 +0100

    Menu de base
    Menu de base sans encore les options reel

--- functions.h
+++ functions.h
@@ -1,1 +1,24 @@
 
+/* Fonction Menu */
+char menu(){
+
+	int c;
+
+	/* Affichage Menu */
+	printf("1.  \n
+			2.	\n
+			3.	\n
+			4.	\n
+			5.	\n
+			0. to Quit	\n");
+
+		c = getchar();
+		/* Suppresion des caracteres dans stdin */
+		if (c != '\n' && c != EOF ){
+			int d;
+			while ( (d = getchar()) != '\n' && d != EOF);
+		}
+
+	}
+return c;
+}
--- main.c
+++ main.c
@@ -4,8 +4,35 @@
 
 int main(int argc, char **argv)
 {
-    //test
+    char menu;
+	menu = menu();
 
+	switch(menu){
+		case '1' :
+		printf("Choisir son menu \n");
+		break;
+		case '2' :
+		printf("Ajouter ingredient \n");
+
+		break;
+		case '3' :
+		printf("Promotion \n");
+
+		break;
+		case '4' :
+		printf("Choisir Table et place \n");
+
+		break;
+		case '5' :
+		printf("Finaliser la commande \n");
+
+		break;
+		case '0' :
+		printf("Quitter \n");
+
+		break;
+		default :
+		printf("Choix par défaut \n");
 
     return 0;
 }
