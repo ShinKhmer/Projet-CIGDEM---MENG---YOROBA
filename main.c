0b67602bdea40c4d3bcfc707d9d70e24538f4916
Asianono <kenjiyoroba@yahoo.fr>
2017-12-19 23:30:59 +0100

    Connexion BDD

--- main.c
+++ main.c
@@ -3,6 +3,29 @@
 #include "ez-draw.h"
 #include "ez-draw_functions.h"
 #include "functions.h"
+#include <winsock.h>
+#include <MYSQL/mysql.h>
+
+// Connection à la BDD
+
+{
+    MYSQL mysql;
+    mysql_init(&mysql);
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    if(mysql_real_connect(&mysql,"host","root","","base_restaurant",0,NULL,0))
+    {
+        mysql_close(&mysql);
+    }
+    else
+    {
+        printf("Une erreur s'est produite lors de la connexion à la BDD!");
+    }
+}
+
+
+
+
 
 void menu(char *);
 
