93f83489eb5232b3e90cc9860dd7defe46dc95fe
Chalana <cmkhmer@gmail.com>
2018-01-14 22:23:25 +0100

    GTK - Onglets créé et boutons en cours

---
+++ lib/functions_gtk.h
@@ -0,0 +1,9 @@
+// PROTOTYPES
+void createWinGTK(GtkWidget *window);
+
+//Creation fenetre , soucis ici au pasage en fonction
+void createWinGTK(GtkWidget *window){
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); /** LA FENETRE SERA POSITIONNEEE AU MILIEU **/
+    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720); /** DIMENSION DE LA FENETRE **/
+    gtk_window_set_title(GTK_WINDOW(window), "CIGMENYO"); /** TITRE DE LA FENETRE **/
+}
--- main.c
+++ main.c
@@ -4,8 +4,23 @@
 #include <winsock.h>
 #include <MYSQL/mysql.h>
 #include <gtk/gtk.h>
-#include "lib\functions.h"
-// PROTOTYPES
+#include "lib/functions.h"
+#include "lib/functions_gtk.h"
+
+
+
+// INIT GTK
+GtkWidget *window;
+GtkWidget *vbox;
+GtkWidget *notebook;
+GtkWidget *vbox2;
+GtkWidget *vbox3;
+GtkWidget *vbox4;
+GtkWidget *vbox5;
+GtkWidget *array;
+GtkWidget *button;
+GtkWidget *pTabLabel;
+gchar *sTabLabel;
 
 int main(int argc, char **argv){
 
@@ -13,17 +28,7 @@ int main(int argc, char **argv){
     int choice_product = 0;
     int line = 0;
 
-    // INIT GTK
-    GtkWidget *window;
-    GtkWidget *vbox;
-    GtkWidget *notebook;
-    GtkWidget *vbox2;
-    GtkWidget *vbox3;
-    GtkWidget *array;
-    GtkWidget *button;
 
-    GtkWidget *pTabLabel;
-    gchar *sTabLabel;
 
     // INIT BDD
     MYSQL *database = NULL;
@@ -75,9 +80,7 @@ int main(int argc, char **argv){
                             /** ETAPE 1 **/
                             /** CREATION DE LA FENETRE **/
                             window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-                            gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); /** LA FENETRE SERA POSITIONNEEE AU MILIEU **/
-                            gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720); /** DIMENSION DE LA FENETRE **/
-                            gtk_window_set_title(GTK_WINDOW(window), "SIGMENYO"); /** TITRE DE LA FENETRE **/
+                            createWinGTK(window);
 
                             /** ETAPE 2 **/
                             vbox = gtk_vbox_new(FALSE, 0); /** CREATION DU BOX ORIENTEE VERTICALEMENT (OU HORIZONTALEMENT SELON VOTRE CHOIX) **/
@@ -97,14 +100,14 @@ int main(int argc, char **argv){
                             array = gtk_table_new(3, 5, TRUE); /** TABLE DE 3 LIGNES 5 COLONNES A VOUS DE CHOISIR **/
                             gtk_box_pack_start(GTK_BOX(vbox2), array, FALSE, FALSE, 10);
 
-                            button = gtk_button_new_with_label("Button 1");
+                            button = gtk_button_new_with_label("Sushi Saumon (Salmon,Rice -1)");
                             gtk_table_attach(GTK_TABLE(array), button, 0, 1, 0, 1, !GTK_EXPAND, !GTK_FILL, 0, 0);
-                            button = gtk_button_new_with_label("Button 2");
+                            button = gtk_button_new_with_label("Brochette Poulet");
                             gtk_table_attach(GTK_TABLE(array), button, 1, 2, 0, 1, !GTK_EXPAND, !GTK_FILL, 0, 0);
+                            /** JOUEZ AVEC LES POSITIONS DES LIGNES ET DES COLONNES POUR LE POSITIONNEMENT DE VOS BOUTONS **/
 
-                                                                    /** JOUEZ AVEC LES POSITIONS DES LIGNES ET DES COLONNES POUR LE POSITIONNEMENT DE VOS BOUTONS **/
 
-                            sTabLabel = g_strdup_printf("Onglet 1"); /** NOM DE VOTRE ONGLET **/
+                            sTabLabel = g_strdup_printf("Menu"); /** NOM DE VOTRE ONGLET **/
                             pTabLabel = gtk_label_new(sTabLabel); /** CREATION D'UN LABEL PERMETTANT DE L'INSERER AU NOTEBOOK **/
 
                             gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox2, pTabLabel); /** INSERTION DE LA PAGE **/
@@ -112,11 +115,24 @@ int main(int argc, char **argv){
                             vbox3 = gtk_vbox_new(FALSE, 0);
                             gtk_container_add(GTK_CONTAINER(window), vbox3);
 
-                            sTabLabel = g_strdup_printf("Onglet 2"); /** NOM DE VOTRE ONGLET2 **/
+                            sTabLabel = g_strdup_printf("Dessert"); /** NOM DE VOTRE ONGLET2 **/
                             pTabLabel = gtk_label_new(sTabLabel); /** CREATION D'UN LABEL PERMETTANT DE L'INSERER AU NOTEBOOK **/
 
                             gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox3, pTabLabel); /** INSERTION DE LA PAGE **/
 
+                            vbox4 = gtk_vbox_new(FALSE, 0);
+                            gtk_container_add(GTK_CONTAINER(window), vbox4);
+
+                            sTabLabel = g_strdup_printf("Ingredient"); /** NOM DE VOTRE ONGLET3 **/
+                            pTabLabel = gtk_label_new(sTabLabel); /** CREATION D'UN LABEL PERMETTANT DE L'INSERER AU NOTEBOOK **/
+                            gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox4, pTabLabel); /** INSERTION DE LA PAGE **/
+
+                            vbox5 = gtk_vbox_new(FALSE, 0);
+                            gtk_container_add(GTK_CONTAINER(window), vbox5);
+
+                            sTabLabel = g_strdup_printf("Table et Finaliser"); /** NOM DE VOTRE ONGLET4 **/
+                            pTabLabel = gtk_label_new(sTabLabel); /** CREATION D'UN LABEL PERMETTANT DE L'INSERER AU NOTEBOOK **/
+                            gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox5, pTabLabel); /** INSERTION DE LA PAGE **/
 
                             /** AFFICHAGE DE TOUS LES ELEMENTS DANS LA FENETRE **/
                             gtk_widget_show_all(window);
