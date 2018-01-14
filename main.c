66b690e8f7b8805c617993154de76e10be5f7574
jojiro <34159710+jojiro@users.noreply.github.com>
2018-01-14 19:47:45 +0100

    de la merde

--- main.c
+++ main.c
@@ -15,6 +15,15 @@ int main(int argc, char **argv){
     GtkApplication *app;
     GtkWidget *button;
     GtkWidget *button_box;
+    GtkWidget *gtk_places_sidebar_new (void);
+
+    typedef struct _GtkPlacesSidebar{
+        gboolean local-only;
+        Gfile * location;
+        GtkPlacesOpenFlags open-flags;
+
+    } GtkPlacesSidebar;
+
     int status;
     int choice = 0;
 
