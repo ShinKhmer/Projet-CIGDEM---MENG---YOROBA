922058eae5eb9defe591b9c829dbe903d6452702
jojiro <34159710+jojiro@users.noreply.github.com>
2018-01-15 00:13:06 +0100

    test button et requete sql
    salmon -1

--- lib/gtk_functions.h
+++ lib/gtk_functions.h
@@ -1,6 +1,6 @@
 // PROTOTYPES
 void createWinGTK(GtkWidget *window);
-static void print_hello (GtkWidget *widget,gpointer data);
+static void print_hello (GtkWidget *widget,gpointer data,MYSQL *db);
 //Creation fenetre , soucis ici au pasage en fonction
 void createWinGTK(GtkWidget *window){
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); /** LA FENETRE SERA POSITIONNEEE AU MILIEU **/
@@ -8,7 +8,9 @@ void createWinGTK(GtkWidget *window){
     gtk_window_set_title(GTK_WINDOW(window), "CIGMENYO"); /** TITRE DE LA FENETRE **/
 }
 
-static void print_hello (GtkWidget *widget,gpointer   data)
+static void print_hello (GtkWidget *widget,gpointer   data,MYSQL *db)
 {
   g_print ("Hello World\n");
+  char update[200] = "UPDATE INGREDIENT SET quantity = quantity - 1 WHERE name = 'salmon'";
+  mysql_query(db, update);
 }
