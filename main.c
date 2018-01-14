8259a7cbd80fc875c22e9460d42b9a4ed35efdc0
jojiro <34159710+jojiro@users.noreply.github.com>
2018-01-14 16:29:49 +0100

    Ajout interface graphique et un bouton

--- main.c
+++ main.c
@@ -6,11 +6,39 @@
 #include <gtk/gtk.h>
 #include "lib\functions.h"
 // PROTOTYPES
+static void activate (GtkApplication* app,gpointer user_data);
+// GTK Function
+static void activate (GtkApplication* app,gpointer user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *button_box;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Bienvenue chez CigMenYo");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
+  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add (GTK_CONTAINER (window), button_box);
+
+  button = gtk_button_new_with_label ("Commencez");
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+  gtk_widget_show_all (window);
+}
+
 
 int main(int argc, char **argv){
-
+    GtkApplication *app;
+    GtkWidget *button;
+    GtkWidget *button_box;
+    int status;
     int choice = 0;
 
+    //GTK WINDOWS APPEL FONCTION
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
     // INITIALISATION BDD
     MYSQL *database = NULL;
     database = mysql_init(database);
